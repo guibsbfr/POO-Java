519b5c2a2f4f69b03038a803646ce9ba467abaca
Guilherme <gui-dev1@hotmail.com>
2026-08-29 10:21:30 -0300

    bad solution

---
+++ Módulo13/Reservation/src/model/entities/Reservation.java
@@ -0,0 +1,69 @@
+package model.entities;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+public class Reservation {
+
+    private Integer roomNumber;
+    private LocalDate checkIn;
+    private LocalDate checkOut;
+
+    DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public Reservation() {
+    }
+
+    public Reservation(Integer roomNumber, LocalDate checkIn, LocalDate checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public LocalDate getCheckIn() {
+        return checkIn;
+    }
+
+    public LocalDate getCheckOut() {
+        return checkOut;
+    }
+
+    public long duration() {
+        return ChronoUnit.DAYS.between(checkIn, checkOut);
+    }
+
+    public String updateDates(LocalDate checkIn, LocalDate checkOut) {
+        LocalDate now = LocalDate.now();
+        if (checkIn.isBefore(now) || checkOut.isBefore(now)) {
+            return "Reservation dates for update must be future dates";
+        }
+        else if (!checkOut.isAfter(checkIn)) {
+            return "Check-out date must be after check-in date";
+        }
+
+       this.checkIn = checkIn;
+       this.checkOut = checkOut;
+       return null;
+    }
+
+    public String toString() {
+        return "Room "
+                + roomNumber
+                + ", check-in: "
+                + fmt.format(checkIn)
+                + ", check-out: "
+                + fmt.format(checkOut)
+                + ", "
+                + duration()
+                + " nights";
+    }
+}
